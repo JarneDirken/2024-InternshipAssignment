8f132f8e44ddfbf0a0f1334061cb19e610debf6d
Jarne Dirken <r0882161@student.thomasmore.be>
2024-03-25 12:11:17 +0700

    made pages only render for certain roles

---
+++ app/api/auth/route.ts
@@ -0,0 +1,32 @@
+// pages/api/auth.js
+import prisma from '@/services/db';
+import { NextApiRequest } from 'next';
+
+export async function POST(req: NextApiRequest) {
+    const { uid } = await new Response(req.body).json();
+
+    const userData = await prisma.user.findUnique({
+        where: {
+            firebaseUid: uid,
+        },
+        include: {
+            role: true,
+        },
+    });
+
+    if (userData) {
+        return new Response(JSON.stringify({ role: userData.role.name }), {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    } else {
+        return new Response(JSON.stringify({ error: 'User not found' }), {
+            status: 404,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    }
+}
