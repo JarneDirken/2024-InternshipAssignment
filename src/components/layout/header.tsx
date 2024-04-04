442ece0771ed022fc7ac7c6d5af099188ea792bb
Jarne Dirken <r0882161@student.thomasmore.be>
2024-04-04 15:08:46 +0700

    borrow to dashboard

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -97,7 +97,7 @@ export default function Header(){
           </Link>
           {user && (
             <Link href="/borrow">
-              Borrow
+              Dashboard
             </Link>
           )}
         </div>
