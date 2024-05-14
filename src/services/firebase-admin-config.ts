fee98b729bf403a865cc27780e0cf6b803f16de5
Jarne Dirken <r0882161@student.thomasmore.be>
2024-05-14 12:50:53 +0700

    changed to use env variables

--- src/services/firebase-admin-config.ts
+++ src/services/firebase-admin-config.ts
@@ -1,10 +1,15 @@
 import admin from 'firebase-admin';
 
-const serviceAccount = require('../config/serviceAccountKey.json');
+const serviceJsonFile = process.env.SERVICE_JSON_FILE;
+if (!serviceJsonFile) {
+    throw new Error('The SERVICE_JSON_FILE environment variable is not set.');
+}
+
+const secretJSON = JSON.parse(serviceJsonFile);
 
 if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
+        credential: admin.credential.cert(secretJSON),
     });
 }
 
